0ffab434df378bce25c53607ad4723336a0bdbc4
MatheusEduardoDasilva <matheus.teteu.silva@gmail.com>
2021-01-18 20:27:41 -0300

    primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Teste_Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://pt.wikipedia.org/wiki/Wikipédia:Página_principal");
+            driver.FindElement(By.Name("search")).SendKeys("pato");
+            driver.FindElement(By.Name("go")).Click();
+            
+        }
+    }
+}
